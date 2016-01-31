5afe1333dd9439db3c4314460e58b17c48346362
yama1968 <ymartel@gmail.com>
2016-01-31 18:58:05 +0100

    init

---
+++ ts.install.sh
@@ -0,0 +1,7 @@
+#
+
+ # GPU
+pip install --upgrade https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.6.0-cp27-none-linux_x86_64.whl
+
+ #CPU
+pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.6.0-cp27-none-linux_x86_64.whl
